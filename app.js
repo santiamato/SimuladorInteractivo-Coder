cea25d32762550e550d4c77ddba723bacff346e8
Santiago Amato <santiamato44@gmail.com>
2022-05-11 01:16:33 -0300

    Agregando eventos

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ let litros;
 let tipoDeConsumible;
 let comida;
 let bebida;
+let contenedor = document.getElementById("container");
+
 
 class productos{
     constructor(nombre, precio, stock, tipo, imagen){
@@ -42,9 +44,9 @@ saludar = () => "Vuelva Pronto!";
 
 mostrarProductos = (mostrar) => {
     listaProductos.filter((el) => el.tipo.includes(mostrar)).forEach((producto) => {
-        let contenedor = document.getElementById("container")
+        let contenedor = document.getElementById("container");
         contenedor.innerHTML = "";
-        let divProducto = document.createElement("div")
+        let divProducto = document.createElement("div");
             divProducto.innerHTML = 
             
             `<div class="row row-cols-1 row-cols-md-3 mb-3 text-center m-5 d-flex justify-content-center">
@@ -56,141 +58,58 @@ mostrarProductos = (mostrar) => {
                         <div class="card-body">
                             <img src="${producto.imagen}" alt="">
                             <h1 class="card-title pricing-card-title">$${producto.precio}<small class="text-muted fw-light">c/u</small></h1>
-                            <button type="button" class="w-100 btn btn-lg btn-primary">Comprar</button>
+                            <label for="btnCantidad"></label>
+                            <input id="btnCantidad" type="number" class="w-100 btn btn-lg m-2" placeholder="Indique Cantidad">
+                            <button id="btnComprar" type="button" class="w-100 btn btn-lg btn-primary">Comprar</button>
                         </div>
                     </div>
                 </div>
             </div>`
-            document.body.append(divProducto)
+            document.body.append(divProducto);
+            
     });
         
 };
 
-let btmNafta = document.getElementById("btmNafta")
-btmNafta.onclick = () => mostrarProductos("NAFTA");
+agregarAlCarrito = () => {
+    let contenedor = document.getElementById("container");
+    contenedor.innerHTML = "";
+    let divCompra = document.createElement("div");
+        divCompra.innerHTML =  
+    `<body class="d-flex h-100 text-center text-white bg-dark" cz-shortcut-listen="true">
+        <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+            <main class="px-3">
+                <h1>Cover your page.</h1>
+                <p class="lead">Cover is a one-page template for building simple and beautiful home pages. Download, edit the text, and add your own fullscreen background photo to make it your own.</p>
+                <p class="lead">
+                <a href="#" class="btn btn-lg btn-secondary fw-bold border-white bg-white">Learn more</a>
+                </p>
+            </main>
+        </div>
+    </body>`
+    document.body.append(divCompra);
+}
 
-let btmTienda = document.getElementById("btmTienda")
-btmTienda.onclick = () => {
+let btnNafta = document.getElementById("btnNafta");
+btnNafta.onclick = () => mostrarProductos("NAFTA");
+
+let btnTienda = document.getElementById("btnTienda");
+btnTienda.onclick = () => {
     mostrarProductos("COMIDA");
     mostrarProductos("BEBIDA");
 };
 
-// do{
-//     let tipoNafta  = (listaProductos.filter((el) => el.tipo.includes("NAFTA")));
-//     let nombresNaftas = tipoNafta.map((el) => el.nombre);
+let btnCantidad = document.getElementById("btnCantidad").value;
+document.addEventListener("DOMContentLoaded", function() {
+    btnCantidad.onkeydown = () => agregarAlCarrito(btnCantidad.value);
+})
 
-//     accion = document.getElementById("btn-nafta");
-//     accion.onclick = ()
+let btnComprar = document.getElementById("btnComprar");
+document.addEventListener("DOMContentLoaded", function() {
+    btnCantidad.onclick = () => agregarAlCarrito();
+})
 
-//     if (accion == listaProductos[1].tipo) {
-//         do{
-//             litros = parseInt(prompt("Cuantos litros?"));
-//             if (litros > listaProductos[1].stock) {
-//                 alert("El maximo de nafta recargable es 50 litros");
-//             }else if (litros <= 0){
-//                 alert("Cargue una cantidad valida");
-//             }
-//         }while ((litros > listaProductos[1].stock) || (litros <= 0));
 
-//         do{
-//             tipoNafta = prompt(`De cual? ${nombresNaftas}`).toUpperCase();
-//             if (tipoNafta != "SUPER" && tipoNafta != "DIESEL" && tipoNafta != "INFINIA") {
-//                 alert("No contamos con ese combustible");
-//             }else{
-//                 alert(`Se le cargara: ${litros} litros de ${tipoNafta}`);
-//                 alert(cargarNafta());
-//                 alert(saludar());
-//                 break;
-//             }
-//         }while (tipoNafta != nombresNaftas);
-
-//     }else if(accion == "TIENDA"){
-//         alert("Bienvenido a la tienda de VSC, tenemos todo lo que nesecita")
-        
-//         tipoDeConsumible = prompt(`Que desea comprar? COMIDA O BEBIDA`).toUpperCase();
-//         if (tipoDeConsumible == listaProductos[3].tipo) {
-            
-//             do{
-//                 let comida = parseInt(prompt(`Tenemos: 1: Pebete  2:Medialuna  3:Sandwich de Miga`));
-//                 switch (comida) {
-//                     case 1:
-//                         alert(`Cada Pebete esta $${pebete.precio}`);
-//                         let cantidadPebete = parseInt(prompt("Cuantos Pebetes desea?"));
-//                         if (cantidadPebete > `${pebete.stock}`) {
-//                             alert("No contamos con esta cantidad: Maximo 30")
-//                         }else if (cantidadPebete <= 0){
-//                             alert("Ingrese una cantidad valida")
-//                         }else{
-//                             alert(`Usted a comprado ${cantidadPebete} pebetes, y le salio en total $${pebete.calcularPrecio(cantidadPebete, pebete.precio)}`)
-//                             alert(saludar());
-//                         }
-//                         break;
-//                     case 2:
-//                         alert(`Cada Medialuna esta $${medialuna.precio}`)
-//                         let cantidadMedialuna = parseInt(prompt("Cuantas medialunas desea?"));
-//                         if (cantidadMedialuna > `${medialuna.stock}`) {
-//                             alert("No contamos con esta cantidad: Maximo 30")
-//                         }else if (cantidadMedialuna <= 0){
-//                             alert("Ingrese una cantidad valida")
-//                         }else{
-//                             alert(`Usted a comprado ${cantidadMedialuna} medialunas, y le salio en total $${medialuna.calcularPrecio(cantidadMedialuna, medialuna.precio)}`)
-//                             alert(saludar());
-//                         }
-//                         break;
-//                     case 3:
-//                         alert(`Cada sandwich de miga esta $${sandwich.precio}`)
-//                         let cantidadSandwich = parseInt(prompt("Cuantos sandwich desea?"));
-//                         if (cantidadSandwich > `${sandwich.stock}`) {
-//                             alert("No contamos con esta cantidad")
-//                         }else if (cantidadSandwich <= 0){
-//                             alert("Ingrese una cantidad valida")
-//                         }else{
-//                             alert(`Usted a comprado ${cantidadSandwich} sandwichs, y le salio en total $${sandwich.calcularPrecio(cantidadSandwich, medialuna.precio)}`)
-//                             alert(saludar());
-//                         }
-//                         break;
-//                     default:
-//                         alert("Escriba el numero de la opcion que quiera.")
-//                         break;
-//                 }
-//             }while((comida != 1) || (comida != 2) || (comida != 3));
-//         }else if(tipoDeConsumible == "BEBIDA"){
-//             do{
-//                 let bebida = parseInt(prompt("Tenemos: 1:Coca Cola, 2:Agua"));
-//                     switch (bebida) {
-//                         case 1:
-//                             alert(`Cada coca cola esta $${cocaCola.precio}`)
-//                             let cantidadCocaCola = parseInt(prompt("Cuantas coca colas desea?"));
-//                             if (cantidadCocaCola > `${cocaCola.stock}`) {
-//                                 alert("No contamos con esta cantidad: Maximo 15")
-//                             }else if (cantidadCocaCola <= 0){
-//                                 alert("Ingrese una cantidad valida")
-//                             }else{
-//                                 alert(`Usted a comprado ${cantidadCocaCola} Coca Colas, y le salio en total $${cocaCola.calcularPrecio(cantidadCocaCola, cocaCola.precio)}`)
-//                                 alert(saludar());
-//                             }
-//                             break;
-//                         case 2:
-//                             alert(`Cada botella de agua esta $${agua.precio}`)
-//                             let cantidadAgua = parseInt(prompt("Cuantas aguas desea?"));
-//                             if (cantidadAgua > `${agua.stock}`) {
-//                                 alert("No contamos con esta cantidad")
-//                             }else if (cantidadAgua <= 0){
-//                                 alert("Ingrese una cantidad valida")
-//                             }else{
-//                                 alert(`Usted a comprado ${cantidadAgua} Aguas, y le salio en total $${agua.calcularPrecio(cantidadAgua, agua.precio)}`)
-//                                 alert(saludar());
-//                             }
-//                             break;
-//                     }
-//             }while((bebida != 1) || (bebida != 2));
-//         }
-//     }else if(accion == "BATHROOM"){
-//         alert("Anda tranki a hacer tus cosas.")
-//     }else {
-//         alert("No contamos con ese servicio.")
-//     }
-// }while ((accion != "NAFTA") || (accion != "TIENDA") || (accion != "BATHROOM"));
 
 
 
